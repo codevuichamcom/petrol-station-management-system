8e7b2d85031405e43f44969460f8a3a0bcac5ba5
lehongquan15a <lehongquan15a@gmail.com>
2021-06-07 11:04:17 +0700

    deploy_18

--- src/main/java/com/gasstation/managementsystem/model/dto/StationDTO.java
+++ src/main/java/com/gasstation/managementsystem/model/dto/StationDTO.java
@@ -1,6 +1,7 @@
 package com.gasstation.managementsystem.model.dto;
 
 import com.gasstation.managementsystem.entity.Station;
+import com.gasstation.managementsystem.entity.User;
 import lombok.*;
 
 import java.util.Optional;
@@ -15,17 +16,20 @@ public class StationDTO {
     private int id;
     private String name;
     private String address;
-    private int ownerId;
-    private String ownerName;
+    private UserDTO owner;
     private int numberOfEmployee;
 
     public StationDTO(Station station) {
         this.id = station.getId();
         this.name = station.getName();
         this.address = station.getAddress();
-        this.ownerId = station.getOwner().getId();
-        this.ownerName = station.getOwner().getName();
-        if(station.getEmployeeList()!=null){
+        User owner = station.getOwner();
+        UserTypeDTO userTypeDTO = new UserTypeDTO(owner.getUserType());
+        this.owner = UserDTO.builder()
+                .id(owner.getId())
+                .name(owner.getName())
+                .userType(userTypeDTO).build();
+        if (station.getEmployeeList() != null) {
             this.numberOfEmployee = station.getEmployeeList().size();
         }
     }

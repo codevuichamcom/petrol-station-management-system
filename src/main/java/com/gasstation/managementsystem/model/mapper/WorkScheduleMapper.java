c6473caa17febcacddad40bfb92d695367da49b4
lehongquan15a <lehongquan15a@gmail.com>
2021-07-11 23:19:28 +0700

    deploy_99.

--- src/main/java/com/gasstation/managementsystem/model/mapper/WorkScheduleMapper.java
+++ src/main/java/com/gasstation/managementsystem/model/mapper/WorkScheduleMapper.java
@@ -1,10 +1,10 @@
 package com.gasstation.managementsystem.model.mapper;
 
-import com.gasstation.managementsystem.entity.Employee;
-import com.gasstation.managementsystem.entity.Shift;
-import com.gasstation.managementsystem.entity.WorkSchedule;
+import com.gasstation.managementsystem.entity.*;
 import com.gasstation.managementsystem.model.dto.employee.EmployeeDTO;
 import com.gasstation.managementsystem.model.dto.shift.ShiftDTO;
+import com.gasstation.managementsystem.model.dto.station.StationDTO;
+import com.gasstation.managementsystem.model.dto.user.UserDTO;
 import com.gasstation.managementsystem.model.dto.workSchedule.WorkScheduleDTO;
 import com.gasstation.managementsystem.model.dto.workSchedule.WorkScheduleDTOCreate;
 import com.gasstation.managementsystem.model.dto.workSchedule.WorkScheduleDTOUpdate;
@@ -18,9 +18,30 @@ public class WorkScheduleMapper {
         if (workSchedule == null) return null;
         Employee employee = workSchedule.getEmployee();
         Shift shift = workSchedule.getShift();
-        EmployeeDTO employeeDTO = employee != null ? EmployeeDTO.builder().id(employee.getId()).name(employee.getName()).build() : null;
-        ShiftDTO shiftDTO = shift != null ? ShiftDTO.builder().id(shift.getId()).name(shift.getName()).build() : null;
+        EmployeeDTO employeeDTO = null;
+        if (employee != null) {
+            Station station = employee.getStation();
+            User owner = station.getOwner();
+            employeeDTO = EmployeeDTO.builder()
+                    .id(employee.getId())
+                    .name(shift.getName())
+                    .station(StationDTO.builder()
+                            .id(station.getId())
+                            .name(station.getName())
+                            .address(station.getAddress())
+                            .owner(UserDTO.builder()
+                                    .id(owner.getId())
+                                    .name(owner.getName()).build())
+                            .build())
+                    .build();
+        }
+        ShiftDTO shiftDTO = shift != null ? ShiftDTO.builder()
+                .id(shift.getId())
+                .name(shift.getName())
+                .startTime(DateTimeHelper.formatDate(shift.getStartTime(), "HH:mm"))
+                .endTime(DateTimeHelper.formatDate(shift.getEndTime(), "HH:mm")).build() : null;
         return WorkScheduleDTO.builder()
+                .id(workSchedule.getId())
                 .employee(employeeDTO)
                 .shift(shiftDTO)
                 .startDate(DateTimeHelper.formatDate(workSchedule.getStartDate(), "yyyy-MM-dd"))

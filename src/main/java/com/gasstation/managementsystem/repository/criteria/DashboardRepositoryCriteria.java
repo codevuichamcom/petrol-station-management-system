2b803f1d99e07606e1d5424ce17e6eaf4a43dc73
lehongquan15a <lehongquan15a@gmail.com>
2021-08-07 00:08:23 +0700

    deploy_167

--- src/main/java/com/gasstation/managementsystem/repository/criteria/DashboardRepositoryCriteria.java
+++ src/main/java/com/gasstation/managementsystem/repository/criteria/DashboardRepositoryCriteria.java
@@ -19,25 +19,34 @@ public class DashboardRepositoryCriteria {
     private final EntityManager em;
 
     public HashMap<String, Object> fuelStatistic(FuelStatisticDTOFilter filter) {
-        String str = "select tbl1.*, tbl2.totalPaid \n" +
+        String str = "select tbl1.*,tbl2.total_debt, tbl3.totalPaid\n" +
                 "from (SELECT ft.id                                           as fuel_id,\n" +
                 "             ft.name                                         AS fuel_name,\n" +
                 "             st.id                                           AS station_id,\n" +
                 "             st.name                                         AS station_name,\n" +
                 "             st.address                                      AS station_address,\n" +
                 "             coalesce(sum(tran.volume * tran.unit_price), 0) as total_revenue,\n" +
-                "             coalesce(sum(dt.accounts_payable), 0)           as total_debt,\n" +
                 "             coalesce(sum(tran.volume), 0)                   as totalVolume\n" +
                 "      FROM transaction_tbl tran\n" +
-                "               right join debt_tbl dt on tran.id = dt.transaction_id\n" +
                 "               RIGHT JOIN pump_shift_tbl pst ON pst.id = tran.pump_shift_id\n" +
                 "               RIGHT JOIN pump_tbl pt ON pt.id = pst.pump_id\n" +
                 "               RIGHT JOIN tank_tbl tt ON tt.id = pt.tank_id\n" +
                 "               right join station_tbl st on st.id = tt.station_id\n" +
                 "               RIGHT JOIN fuel_tbl ft ON ft.id = tt.fuel_id\n" +
-                "      where tran.time between :startTime and :endTime\n" +
-                "         or tran.time is null\n" +
+                "      where (tran.time between :startTime and :endTime\n" +
+                "          or tran.time is null)\n" +
                 "      GROUP BY ft.id, ft.name, st.id, st.name, st.address) as tbl1,\n" +
+                "     (select ft.id as fuel_id, coalesce(sum(tran.volume * tran.unit_price), 0) as total_debt\n" +
+                "      FROM debt_tbl dt\n" +
+                "               right join transaction_tbl tran on tran.id = dt.transaction_id\n" +
+                "               RIGHT JOIN pump_shift_tbl pst ON pst.id = tran.pump_shift_id\n" +
+                "               RIGHT JOIN pump_tbl pt ON pt.id = pst.pump_id\n" +
+                "               RIGHT JOIN tank_tbl tt ON tt.id = pt.tank_id\n" +
+                "               right join station_tbl st on st.id = tt.station_id\n" +
+                "               RIGHT JOIN fuel_tbl ft ON ft.id = tt.fuel_id\n" +
+                "      where (tran.time between :startTime and :endTime\n" +
+                "          or tran.time is null)\n" +
+                "      GROUP BY ft.id) as tbl2,\n" +
                 "     (\n" +
                 "         select ft.id                                                                            as fuel_id,\n" +
                 "                coalesce(sum(tt.volume * tt.unit_price - coalesce((dt.accounts_payable), 0)), 0) as totalPaid\n" +
@@ -48,12 +57,12 @@ public class DashboardRepositoryCriteria {
                 "                  right join pump_tbl pt on pt.id = pst.pump_id\n" +
                 "                  right join tank_tbl t on t.id = pt.tank_id\n" +
                 "                  right join fuel_tbl ft on ft.id = t.fuel_id\n" +
-                "         where tt.time between :startTime and :endTime\n" +
-                "            or tt.time is null\n" +
+                "         where (tt.time between :startTime and :endTime\n" +
+                "             or tt.time is null)\n" +
                 "         GROUP BY ft.id)\n" +
-                "         as tbl2\n" +
-                "where tbl1.fuel_id = tbl2.fuel_id\n" +
-                "  and tbl1.station_id is not null";
+                "         as tbl3\n" +
+                "where tbl1.fuel_id = tbl2.fuel_id and tbl2.fuel_id = tbl3.fuel_id\n" +
+                "  and tbl1.station_id is not null\n";
         if (filter.getStationId() != null) {
             str += "  and tbl1.station_id = :stationId";
         }
@@ -74,8 +83,8 @@ public class DashboardRepositoryCriteria {
                     .stationName((String) objects[3])
                     .stationAddress((String) objects[4])
                     .totalRevenue((Double) objects[5])
-                    .totalDebt((Double) objects[6])
-                    .totalVolume((Double) objects[7])
+                    .totalVolume((Double) objects[6])
+                    .totalDebt((Double) objects[7])
                     .totalPaid((Double) objects[8])
                     .build();
             fuelStatistics.add(revenue);

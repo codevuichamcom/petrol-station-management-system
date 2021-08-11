37c34036d4ce4412dacd65e41adbc8d8b79e8131
lehongquan15a <lehongquan15a@gmail.com>
2021-08-11 11:48:55 +0700

    deploy_205

--- src/main/java/com/gasstation/managementsystem/repository/criteria/PumpShiftRepositoryCriteria.java
+++ src/main/java/com/gasstation/managementsystem/repository/criteria/PumpShiftRepositoryCriteria.java
@@ -93,7 +93,7 @@ public class PumpShiftRepositoryCriteria {
         Query volumeAmountQuery = em.createQuery(totalVolumeAndAmountQuery);
         volumeAmountHelper.setValueToParams(volumeAmountQuery);
         Object[] volumeAndAmount = (Object[]) volumeAmountQuery.getSingleResult();
-        qHelper.sort("ps.createdDate", "id");
+        qHelper.sort("ps.id", "DESC");
         TypedQuery<PumpShift> tQuery = em.createQuery(qHelper.getQuery().toString(), PumpShift.class);
         HashMap<String, Object> map = qHelper.paging(tQuery, countTotalQuery, filter.getPageIndex(), filter.getPageSize());
         map.put("totalVolume", volumeAndAmount[0]);

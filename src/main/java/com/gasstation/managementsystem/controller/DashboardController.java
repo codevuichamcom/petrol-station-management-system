233e6260616f4b9b663f1fa7d3e260fb458d84bb
lehongquan15a <lehongquan15a@gmail.com>
2021-08-19 10:11:27 +0700

    deploy_252

--- src/main/java/com/gasstation/managementsystem/controller/DashboardController.java
+++ src/main/java/com/gasstation/managementsystem/controller/DashboardController.java
@@ -1,11 +1,8 @@
 package com.gasstation.managementsystem.controller;
 
 import com.gasstation.managementsystem.entity.Api;
-import com.gasstation.managementsystem.entity.User;
-import com.gasstation.managementsystem.entity.UserType;
 import com.gasstation.managementsystem.model.dto.dashboard.FuelStatisticDTOFilter;
 import com.gasstation.managementsystem.model.dto.dashboard.TankStatisticDTOFilter;
-import com.gasstation.managementsystem.model.dto.station.StationDTO;
 import com.gasstation.managementsystem.service.DashboardService;
 import com.gasstation.managementsystem.service.StationService;
 import com.gasstation.managementsystem.utils.UserHelper;
@@ -15,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = Api.PREFIX)
@@ -32,18 +28,6 @@ public class DashboardController {
     public HashMap<String, Object> fuelStatistic(@RequestParam(name = "startTime") Long startTime,
                                                  @RequestParam(name = "endTime") Long endTime,
                                                  @RequestParam(name = "stationIds", required = false) Integer[] stationIds) {
-        User userLoggedIn = userHelper.getUserLogin();
-        if (userLoggedIn.getUserType().getId() == UserType.OWNER && (stationIds == null || stationIds.length == 0)) {
-            HashMap<String, Object> map = stationService.findAll();
-            List<StationDTO> stationDTOList = (List<StationDTO>) map.get("data");
-            stationIds = new Integer[100];
-            if (stationDTOList != null && stationDTOList.size() != 0) {
-                for (int i = 0; i < stationDTOList.size(); i++) {
-                    StationDTO stationDTO = stationDTOList.get(i);
-                    stationIds[i] = stationDTO.getId();
-                }
-            }
-        }
         FuelStatisticDTOFilter filter = FuelStatisticDTOFilter.builder()
                 .startTime(startTime)
                 .endTime(endTime).stationIds(stationIds).build();

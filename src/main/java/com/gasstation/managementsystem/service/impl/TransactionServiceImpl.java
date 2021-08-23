38dd0d9c7de944ef250db16df18b914ff546c4d7
lehongquan15a <lehongquan15a@gmail.com>
2021-08-23 10:43:49 +0700

    deploy_266

--- src/main/java/com/gasstation/managementsystem/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/gasstation/managementsystem/service/impl/TransactionServiceImpl.java
@@ -20,6 +20,7 @@ import com.gasstation.managementsystem.utils.UserHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
